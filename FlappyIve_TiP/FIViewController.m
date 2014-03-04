9e0da7c93d05e13b61f202494d333d04c81e1a90
Kyle Frost <kyle@kylefrostdesign.com>
2014-03-03 19:00:12 -0500

    Add comments for tutorial

--- FlappyIve_TiP/FIViewController.m
+++ FlappyIve_TiP/FIViewController.m
@@ -39,20 +39,21 @@
     NSArray *jonyArray = [NSArray arrayWithObjects:[UIImage imageNamed:@"jony0.png"], [UIImage imageNamed:@"jony1.png"], [UIImage imageNamed:@"jony2.png"], [UIImage imageNamed:@"jony3.png"], nil];
     
     [jonyFlapper setAnimationImages:jonyArray];
-    [jonyFlapper setAnimationDuration:0.7];
+    [jonyFlapper setAnimationDuration:0.5];
     [jonyFlapper startAnimating];
     [[self view] addSubview:jonyFlapper];
     [self setJony:jonyFlapper];
     
     // *********** 3 *********** //
-    UILabel *score = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, CGRectGetWidth([[self view] frame]), 80)];
+    UILabel *score = [[UILabel alloc] initWithFrame:CGRectMake(0, 40, CGRectGetWidth([[self view] frame]), 80)];
     [score setTextAlignment:NSTextAlignmentCenter];
-    [score setFont:[UIFont systemFontOfSize:80.0f]];
+    [score setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:40.0]];
     [[self view] addSubview:score];
     [self setScoreLabel:score];
     
     [self setScotts:[NSMutableArray array]];
     
+    // *********** 4 *********** //
     [self startTimers];
     
 }
@@ -60,11 +61,13 @@
 
 -(void)startTimers {
     
+    // *********** 1 *********** //
     CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(tick:)];
     [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     [self setDisplayLink:link];
     
-    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:ScottDelay target:self selector:@selector(addNewBar:) userInfo:nil repeats:YES];
+    // *********** 2 *********** //
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:ScottDelay target:self selector:@selector(addNewScott:) userInfo:nil repeats:YES];
     [self setScottTimer:timer];
     
 }
@@ -72,10 +75,12 @@
 
 -(void)tick:(CADisplayLink *)link {
     
+    // *********** 1 *********** //
     self.jonyVelocity += [link duration]*JonyAcceleration;
     
     [[self jony] setFrame:CGRectOffset([[self jony] frame], 0, [self jonyVelocity])];
     
+    // *********** 2 *********** //
     UIView *removeScott = nil;
     for (UIView *scott in [self scotts]) {
         [scott setFrame:CGRectOffset([scott frame], [link duration]*HorizVelocity, 0)];
@@ -89,7 +94,7 @@
             [scott setTag:1];
             [self incrementCount];
         }
-        
+
         if (CGRectGetMaxX([scott frame]) < 0)
             removeScott = scott;
     }
@@ -106,6 +111,7 @@
 
 -(void)incrementCount {
     
+    // *********** 1 *********** //
     NSInteger currentScore = [[[[self scoreLabel] attributedText] string] intValue];
     NSDictionary *scoreLabelAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
     NSAttributedString *attributes = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%i", currentScore+1] attributes:scoreLabelAttributes];
@@ -114,37 +120,9 @@
     
 }
 
-
--(void)addNewBar:(NSTimer *)timer {
-    
-    NSInteger possibleScottRange = CGRectGetHeight([[self view] frame])-ScottHeight-2*TopBottomPadding;
-    NSInteger scottTop = arc4random()%possibleScottRange + TopBottomPadding;
-    
-    UIImageView *newTopScott = [[UIImageView alloc] initWithFrame:CGRectMake(CGRectGetWidth([[self view] frame]), 0, 50, scottTop)];
-    [newTopScott setImage:[[UIImage imageNamed:@"scott_upside_down.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 20, 0)]];
-    [newTopScott setTag:1];
-    
-    CGFloat scottBottom = scottTop+ScottHeight;
-    UIImageView *newBottomScott = [[UIImageView alloc] initWithFrame:CGRectMake(CGRectGetWidth([[self view] frame]), scottBottom, 50, CGRectGetHeight([[self view] frame])-scottBottom)];
-    [newBottomScott setImage:[[UIImage imageNamed:@"scott.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(20, 0, 0, 0)]];
-    
-    [[self view] insertSubview:newTopScott belowSubview:[self scoreLabel]];
-    [[self view] insertSubview:newBottomScott belowSubview:[self scoreLabel]];
-    
-    [[self scotts] addObject:newTopScott];
-    [[self scotts] addObject:newBottomScott];
-    
-}
-
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    
-    [self setJonyVelocity:TapJonyVelocity];
-    
-}
-
 -(void)failed {
     
+    // *********** 2 *********** //
     [[self scottTimer] invalidate];
     [self setScottTimer:nil];
     
@@ -157,18 +135,49 @@
 
 -(void)startOver {
     
+    // *********** 3 *********** //
     [[self scoreLabel] setAttributedText:nil];
     
     for (UIView *scott in [self scotts]) {
         [scott removeFromSuperview];
     }
+    // *********** 4 *********** //
     [[self scotts] removeAllObjects];
-    
     [[self jony] setFrame:JonyStartingFrame];
-    
     [self setJonyVelocity:0];
-    
     [self startTimers];
+    
+}
+
+-(void)addNewScott:(NSTimer *)timer {
+    
+    // *********** 1 *********** //
+    NSInteger possibleGapRange = CGRectGetHeight([[self view] frame])-ScottHeight-2*TopBottomPadding;
+    NSInteger scottTop = arc4random()%possibleGapRange + TopBottomPadding;
+    
+    // *********** 2 *********** //
+    UIImageView *newTopScott = [[UIImageView alloc] initWithFrame:CGRectMake(CGRectGetWidth([[self view] frame]), 0, 50, scottTop)];
+    [newTopScott setImage:[[UIImage imageNamed:@"scott_upside_down.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 20, 0)]];
+    [newTopScott setTag:1];
+    
+    CGFloat scottBottom = scottTop+ScottHeight;
+    UIImageView *newBottomScott = [[UIImageView alloc] initWithFrame:CGRectMake(CGRectGetWidth([[self view] frame]), scottBottom, 50, CGRectGetHeight([[self view] frame])-scottBottom)];
+    [newBottomScott setImage:[[UIImage imageNamed:@"scott.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(20, 0, 0, 0)]];
+    
+    // *********** 3 *********** //
+    [[self view] insertSubview:newTopScott belowSubview:[self scoreLabel]];
+    [[self view] insertSubview:newBottomScott belowSubview:[self scoreLabel]];
+    
+    [[self scotts] addObject:newTopScott];
+    [[self scotts] addObject:newBottomScott];
+    
+}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    // *********** 1 *********** //
+    [self setJonyVelocity:TapJonyVelocity];
     
 }
 
